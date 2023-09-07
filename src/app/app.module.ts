f3f12238b0cbcee449d15e272979a3881bd0b84f
ajayreddy002 <ajaykumarreddy.sanivarapu@gmail.com>
2023-09-07 12:23:41 +0530

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,40 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FontAwesomeModule, FaIconLibrary } from '@fortawesome/angular-fontawesome';
+import { faAngleRight, faArrowRight, faEnvelope, faHome, faMobilePhone, faPhone, faPhoneAlt } from '@fortawesome/free-solid-svg-icons';
+import { HomeComponent } from './home/home.component';
+import { HomeBannerComponent } from './home-banner/home-banner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    HomeBannerComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    FontAwesomeModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(library: FaIconLibrary) {
+    library.addIcons(
+      faAngleRight,
+      faHome,
+      faEnvelope,
+      faPhone,
+      faMobilePhone,
+      faArrowRight
+    )
+  }
+ }
