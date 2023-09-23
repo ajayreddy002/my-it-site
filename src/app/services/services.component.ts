0dc216f39785dd08d34638208e67e59b9be643bc
ajayreddy002 <ajaykumarreddy.sanivarapu@gmail.com>
2023-09-23 12:34:10 +0530

    Services

--- src/app/services/services.component.ts
+++ src/app/services/services.component.ts
@@ -10,26 +10,38 @@ export class ServicesComponent {
   serviceList = [
     {
       title:'Wed App Development',
-      description:'Convert data noise to intelligent insights for competitive differention quality.',
-      img:'mobile-app.png',
+      description:'In Varcas Info we build websites and applications according to client requirement.',
+      img:'web-development.png',
       url:'/web-app'
     },
     {
       title:'Mobile App Development',
-      description:'Convert data noise to intelligent insights for competitive differention quality.',
-      img:'mobile-app.png',
+      description:'In Varcas Info we build Mobile applications according to client requirement.',
+      img:'mobile-app.jpeg',
       url:'/mobile-app'
     },
     {
-      title:'Wed App Development',
+      title:'SaaS Development',
       description:'Convert data noise to intelligent insights for competitive differention quality.',
-      img:'mobile-app.png',
+      img:'sass-development.jpeg',
       url:'/web-app'
     },
     {
-      title:'Wed App Development',
-      description:'Convert data noise to intelligent insights for competitive differention quality.',
-      img:'mobile-app.png',
+      title:'UX/UI Design',
+      description:'In Varcas Info we do design the UX/UI according to client requirement.',
+      img:'ux-ui-design.jpeg',
+      url:'/web-app'
+    },
+    {
+      title:'Manual Testing',
+      description:'In Varcas Info we do Manual test of web and mobile application and will do Regression Testing and write Test cases.',
+      img:'Manual-testing.jpeg',
+      url:'/web-app'
+    },
+    {
+      title:'Automation Testing',
+      description:'In Varcas Info we will write automated scripting for Test Senarios.',
+      img:'Automation_Testing.jpeg',
       url:'/web-app'
     },
   ]
